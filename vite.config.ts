55ed2e48c4b88adf1ab63d77d1a87532f4d39043
namNguyenThanh1511 <nam151103@gmail.com>
2025-02-20 01:38:00 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/meraki", //same with repo name for mapping pages in github deploy
+  base: "", //same with repo name for mapping pages in github deploy
 });
